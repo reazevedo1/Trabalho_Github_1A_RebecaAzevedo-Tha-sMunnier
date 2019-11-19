190a60e6825b8fa8df7d86df1776b2e9457dbc32
reazevedo1 <57267889+reazevedo1@users.noreply.github.com>
2019-11-19 15:49:55 -0300

    Update projetoGIT1.cpp
    Fiz as duas primeiras funções e testei no main.
    Bjss.
    Rebeca Azevedo

--- projetoGIT1.cpp
+++ projetoGIT1.cpp
@@ -4,15 +4,31 @@
 /*
 	Conta todas as letras 'letra' contidas em 'texto'
 */
-int contaLetras(char letra, string texto)
+
+int conta_letra (string palavra)
 {
+int qtd;
+qtd=palavra.length();
+return qtd;	
+}
+
 }
 
 /*
 	Substitui todas as ocorrencias de 'letra1' por 'letra2' contidas em 'texto'
 */
-void substituirLetra(char letra1,char letra2, string texto)
+string substituirLetra(char letra1,char letra2, string texto)
 {
+	int conta=conta_letra(texto);
+	int i;
+	for(i=0; i<conta; i=i+1)
+	{
+		if(texto[i]==letra1)
+		{
+			texto[i]=letra2;
+		}
+	}
+	return texto;
 }
 
 /*
@@ -30,9 +46,25 @@ void substituirPalavras(string palavra1, string palavra2, string texto)
 }
 
 int main(){
-
-	cout << "Entre com o texto para fazer o teste:";
-
+	
+	int r;
+	string p;
+	cout<<"Digite a palavra: \n";
+	getline(cin,p);
+	r=conta_letra(p);
+	cout<<r<<"\n\n\n";
+	
+	string t, tex; 
+	char a;
+	char b;
+	cout<<"texto: \n";
+	getline(cin, t);
+	cout<<"letra1: \n";
+	cin>>a;
+	cout<<"letra2: \n";
+	cin>>b;
+	tex=substituirLetra(a, b, t);
+	cout<<tex<<;
 
 
 
