5fa82b5b20cf096468e2a361b6682a6406d62c80
reazevedo1 <57267889+reazevedo1@users.noreply.github.com>
2019-11-20 23:33:53 -0300

    Update projetoGIT1.cpp
    atualizei as funções1 e 2 e add comentários
    att, Rebeca A.

--- projetoGIT1.cpp
+++ projetoGIT1.cpp
@@ -1,73 +1,75 @@
-#include <iostream>
-#include <string>
-using namespace std;
+# include  < iostream >
+# include  < string >
+usando  espaço para nome  std ;
 
-/*
+/ *
 	Conta todas as letras 'letra' contidas em 'texto'
-*/
+* /
 
-int conta_letra (string palavra)
+int  conta_letra (string palavra)
 {
 int qtd;
-qtd=palavra.length();
+qtd = palavra. length ();
 return qtd;	
 }
 
 }
 
-/*
-	Substitui todas as ocorrencias de 'letra1' por 'letra2' contidas em 'texto'
-*/
-string substituirLetra(char letra1,char letra2, string texto)
+/ *
+	Substitui todas as ocorrências de 'letra1' por 'letra2' contidas em 'texto'
+* /
+string substituirLetra ( char letra1, char letra2, string texto)
 {
-	int conta=conta_letra(texto);
+	int conta = conta_letra (texto);
 	int i;
-	for(i=0; i<conta; i=i+1)
+	para (i = 0 ; i <conta; i = i + 1 )
 	{
-		if(texto[i]==letra1)
+		if (texto [i] == letra1)
 		{
-			texto[i]=letra2;
+			texto [i] = letra2;
 		}
 	}
-	return texto;
+	retornar texto;
 }
 
-/*
-	Conta todas as ocorrencias de 'palavra' contidas em 'texto'
-*/
-int contaPalavras(string palavra, string texto)
+/ *
+	Conta todas as ocorrências de 'palavra' contidas em 'texto'
+* /
+int  contaPalavras (string palavra, string texto)
 {
 }
 
-/*
-	Substitui todas as ocorrencias de 'palavra1' por 'palavra2' em 'texto'
-*/
-void substituirPalavras(string palavra1, string palavra2, string texto)
+/ *
+	Substitui todas as ocorrências de 'palavra1' por 'palavra2' em 'texto'
+* /
+void  substituirPalavras (string palavra1, string palavra2, string texto)
 {
 }
 
-int main(){
+int  main () {
 	
+	//respectivo a funcao1
 	int r;
 	string p;
-	cout<<"Digite a palavra: \n";
-	getline(cin,p);
-	r=conta_letra(p);
-	cout<<r<<"\n\n\n";
+	cout << " Digite a palavra: \ n " ;
+	getline (cin, p);
+	r = conta_letra (p);
+	cout << r << " \ n \ n \ n " ;
 	
+	//respectivo a funcao2
 	string t, tex; 
 	char a;
 	char b;
-	cout<<"texto: \n";
-	getline(cin, t);
-	cout<<"letra1: \n";
-	cin>>a;
-	cout<<"letra2: \n";
-	cin>>b;
-	tex=substituirLetra(a, b, t);
-	cout<<tex<<"\n\n\n";
+	cout << " texto: \ n " ;
+	getline (cin, t);
+	cout << " letra1: \ n " ;
+	cin >> a;
+	cout << " letra2: \ n " ;
+	cin >> b;
+	tex = substituirLetra (a, b, t);
+	cout << tex << " \ n \ n \ n " ;
 
 
 
-	return 0;
+	retornar  0 ;
 }
